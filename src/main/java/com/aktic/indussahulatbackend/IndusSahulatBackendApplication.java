c81c65c22798cb2da2453c92bdbf84eb498adffc
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-03-07 14:30:03 +0500

    Project setup

---
+++ src/main/java/com/aktic/indussahulatbackend/IndusSahulatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aktic.indussahulatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndusSahulatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndusSahulatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aktic/indussahulatbackend/IndusSahulatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktic.indussahulatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndusSahulatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
